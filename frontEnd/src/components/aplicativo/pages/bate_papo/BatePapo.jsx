edd56e6be14c5b7399bf0e86e1b5117b0d3f3199
Ramon Rodrigues <ramon.rodrigues.dev@gmail.com>
2024-08-29 19:57:47 -0300

    iniciando customização dos cards de cursos

--- frontEnd/src/components/aplicativo/pages/bate_papo/BatePapo.jsx
+++ frontEnd/src/components/aplicativo/pages/bate_papo/BatePapo.jsx
@@ -20,8 +20,22 @@ function BatePapo() {
       </div>
 
       <div className={styles.containerDeCardsDeGrupos}>
-        <div className={styles.cardDeGrupos}>Aqui ficara os grupos</div>
-        <div className={styles.cardDeGrupos}>Vazio</div>
+        <div className={styles.cardDeGrupos}>
+          <img src="https://i.pinimg.com/originals/e4/01/27/e40127c71b4a07bb4eb02cd62f711dbb.jpg" alt="" />
+
+          <div className={styles.textCard}>
+              <h2>JAVASCRIPT</h2>
+              <p>[ Fundamentos I ]</p>
+          </div>
+        </div>
+        <div className={styles.cardDeGrupos}>
+          <img src="https://i.pinimg.com/originals/ab/5d/55/ab5d5545f0f0ae703a77c023074edc34.jpg" alt="" />
+          
+          <div className={styles.textCard}>
+              <h2>JAVASCRIPT</h2>
+              <p>[ Fundamentos II ]</p>
+          </div>
+        </div>
         <div className={styles.cardDeGrupos}>Vazio</div>
         <div className={styles.cardDeGrupos}>Vazio</div>
       </div>
